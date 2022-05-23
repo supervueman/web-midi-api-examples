ce16dfe3f1fc3d0172b34c806cf925f0cf8463d2
supervueman <supervueman@gmail.com>
2022-05-23 16:33:38 +0400

    Add event on connect midi device and handler on key down

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,7 +11,35 @@ window.addEventListener('load', () => {
 
   function success(midiAccess) {
     console.log('Success')
+    // Если не подключено миди устройстов то inputs size будет 0
     console.log(midiAccess)
+
+    // Вызывается при включении или отключении миди устройства
+    midiAccess.addEventListener('statechange', updateDevices);
+
+    const inputs = midiAccess.inputs
+
+    inputs.forEach(input => {
+      console.log(input)
+      input.onmidimessage = handleInput
+    })
+  }
+
+  function updateDevices(event) {
+    console.log(event)
+    setMidiDeviceName(event.port?.name)
+  }
+
+  function setMidiDeviceName(name) {
+    if (name) {
+      $('.title').text(name)
+    } else {
+      $('.title').text('Unknown')
+    }
+  }
+
+  function handleInput(event) {
+    console.log(event)
   }
 
   function failure() {
