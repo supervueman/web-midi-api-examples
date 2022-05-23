7fed34026769ebf46dbe27090d1e689460f58209
supervueman <supervueman@gmail.com>
2022-05-23 16:05:44 +0400

    Add midi access condition

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,4 +5,16 @@ import './vendor/fontawesome'
 
 window.addEventListener('load', () => {
   console.log('load')
+  if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess().then(success, failure);
+  }
+
+  function success(midiAccess) {
+    console.log('Success')
+    console.log(midiAccess)
+  }
+
+  function failure() {
+    console.log('Failed')
+  }
 })
